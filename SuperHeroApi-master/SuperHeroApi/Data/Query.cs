7fa7ee298b3d96f88094bd553a185d334fe13e0f
diarbelo <dbetancur@growthaccelerationpartners.com>
2023-08-30 22:03:34 -0500

    Implementing queries

--- SuperHeroApi-master/SuperHeroApi/Data/Query.cs
+++ SuperHeroApi-master/SuperHeroApi/Data/Query.cs
@@ -4,9 +4,6 @@ namespace SuperHeroApi.Data
 {
     public class Query
     {
-        [UseProjection]
-        [UseFiltering]
-        [UseSorting]
         public IQueryable<Superhero> GetSuperheroes([Service] ApplicationDbContext context) =>
             context.Superheroes;
     }
